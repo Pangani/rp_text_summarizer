5fc8e34659ce419ba189bc066d116f54768beef4
Pangani <roypangani@gmail.com>
2024-02-22 12:58:26 +0000

    data validation componet added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from text_summarizer.pipeline.stagee_01_data_ingestion import DataIngestionTrainingPipeline
+from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from text_summarizer.logging import logger
 
 logger.info("Welcome to custom text summarizer package")
@@ -9,6 +10,16 @@ try:
     logger.info(f">>>>>> Starting {STAGE_NAME}... ")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed successfully.")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "STAGE_02_DATA_VALIDATION"
+try:
+    logger.info(f">>>>>> Starting {STAGE_NAME}... ")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> {STAGE_NAME} completed successfully.")
 except Exception as e:
     logger.exception(e)
--- src/text_summarizer/config/configuration.py
+++ src/text_summarizer/config/configuration.py
@@ -1,6 +1,7 @@
 from text_summarizer.constants import *
 from text_summarizer.utils.common import read_yaml, create_directory
-from text_summarizer.entity import (DataIngestionConfig)
+from text_summarizer.entity import (DataIngestionConfig, 
+                                    DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
@@ -20,4 +21,17 @@ class ConfigurationManager:
             unzip_dir = config.unzip_dir
         )
 
-        return data_ingestion_config   
+        return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directory([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config 
